00fe41c73cce3576f9a6086f38aa42cecf072a5b
Ana Margarida Borges <a252137@dac.unicamp.br>
2025-08-13 10:16:55 -0300

    primeiro commit

---
+++ Satiros/Satiros macOS/AppDelegate.swift
@@ -0,0 +1,29 @@
+//
+//  AppDelegate.swift
+//  Satiros macOS
+//
+//  Created by Ana Margarida Diniz Silva Borges on 13/08/25.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
