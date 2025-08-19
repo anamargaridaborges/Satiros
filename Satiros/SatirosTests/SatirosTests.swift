d1d418e3218072d83acf881976e462798b25ab65
Ana Margarida Borges <a252137@dac.unicamp.br>
2025-08-19 10:55:23 -0300

    project.yml

---
+++ Satiros/SatirosTests/SatirosTests.swift
@@ -0,0 +1,16 @@
+//
+//  SatirosTests.swift
+//  SatirosTests
+//
+//  Created by Ana Margarida Diniz Silva Borges on 19/08/25.
+//
+
+import Testing
+
+struct SatirosTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
