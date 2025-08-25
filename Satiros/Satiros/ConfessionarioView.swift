c8b6d76ade46d2e5dfae59cd3242bef68032bf0e
jordana-santos <jordanalsantos@hotmail.com>
2025-08-25 11:51:23 -0300

    inicio tela confessionario

--- Satiros/Satiros/ConfessionarioView.swift
+++ Satiros/Satiros/ConfessionarioView.swift
@@ -15,14 +15,57 @@ struct ConfessionarioView: View {
 	
     var body: some View {
 			VStack {
-				Text("Confessionário")
-					.font(.appFont(selectedFont, size:60))
-				Text(String(contexto.popularidade))
-					.font(.appFont(selectedFont,size:60))
+				HStack{
+					Circle()
+						.frame(width: 50, height: 50)
+						.padding(.leading, 10)
+					
+					Spacer()
+					Text("Day \(contexto.dia) - Morning")
+						.font(.appFont(selectedFont,size:30))
+					
+					Spacer()
+					VStack{
+						Rectangle() //popularidade
+							.frame(width: 200, height: 30)
+							.padding(.top, 10)
+							.padding(.trailing, 20)
+						
+						Rectangle()//desconfiança
+							.frame(width: 200, height: 30)
+							.padding(.trailing, 20)
+					}
+				}
+				HStack{ //bloco de notas
+					Spacer()
+					Rectangle()
+						.frame(width: 120, height: 120)
+						.padding(20)
+				}
+				
+	
+				Spacer()
+				Rectangle()
+						.frame(width: 800, height: 200)
+						.overlay(
+								Text("texto da confissao")
+										.font(.appFont(selectedFont, size: 30))
+										.foregroundColor(.black),
+								alignment: .center
+						)
+						.overlay(
+								Rectangle()
+										.frame(width: 40, height: 30)
+										.foregroundColor(.gray)
+										.padding(10),
+								alignment: .bottomTrailing
+						)
+						.padding(.bottom, 50)
+
 			}
 			.navigationBarBackButtonHidden()
 			.frame(maxWidth: .infinity, maxHeight: .infinity)
-    }
+		}
 }
 
 #Preview {
