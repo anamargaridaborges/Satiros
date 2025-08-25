a290c3915a07811afc5aa9bcc3c9d7c44e03d163
Ana Margarida Borges <a252137@dac.unicamp.br>
2025-08-25 09:12:41 -0300

    continuar associacao id e contexto

---
+++ Satiros/Satiros/AssociaIdContexto.swift
@@ -0,0 +1,20 @@
+//
+//  AssociaIdContexto.swift
+//  Satiros
+//
+//  Created by Ana Margarida Diniz Silva Borges on 25/08/25.
+//
+
+import Foundation
+
+func retornaID (dia: Int, horario: String, local: String) -> Int {
+	if (dia == 1 && horario == "confissao1" && local == "confessionario") {
+		return 15
+	}
+	else if (dia == 1 && horario == "confissao2" && local == "confessionario") {
+		return 13
+	}
+	else {
+		return -1
+	}
+}
