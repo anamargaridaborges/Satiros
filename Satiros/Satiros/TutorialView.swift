0a8182b0073bce6a69f339649dcf3cc03daf7e8e
Ana Margarida Borges <a252137@dac.unicamp.br>
2025-08-25 10:38:22 -0300

    texto animado

--- Satiros/Satiros/TutorialView.swift
+++ Satiros/Satiros/TutorialView.swift
@@ -13,12 +13,13 @@ struct TutorialView: View {
 	@Bindable var contexto: ContextoSalvo
 	@Binding var path: [String]
 	@FocusState private var estaFocado: Bool
+	@State var texto: String = ""
 	
     var body: some View {
 			VStack {
-				Text(dialogos[contexto.idDialogo ?? 0].personagem)
+				Text(dialogos[contexto.idDialogo ?? 0].personagem + ":")
 					.font(.appFont(selectedFont, size:60))
-				Text(dialogos[contexto.idDialogo ?? 0].texto[0])
+				Text(texto)
 					.font(.appFont(selectedFont, size:40))
 			}
 			.padding()
@@ -27,14 +28,30 @@ struct TutorialView: View {
 			.focused($estaFocado)
 			.onKeyPress(.return) {
 				contexto.idDialogo = dialogos[contexto.idDialogo ?? 0].id_que_opcao_leva[0]
+				animacaoTexto()
 				return .handled
 			}
 			.onAppear {
 				estaFocado = true
+				animacaoTexto()
 			}
 			.frame(maxWidth: .infinity, maxHeight: .infinity)
 			.navigationBarBackButtonHidden()
     }
+	
+	func animacaoTexto(posicao: Int = 0) {
+		if posicao == 0 {
+			texto = ""
+		}
+		let fala = dialogos[contexto.idDialogo ?? 0].texto[0]
+		Task {
+			for c in fala {
+				texto.append(c)
+				try? await Task.sleep(nanoseconds: 50_000_000)
+			}
+		}
+	}
+	
 }
 
 #Preview {
