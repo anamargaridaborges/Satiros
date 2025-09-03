783f128c4e97384be230c2a8b1b2da5927832462
jordana-santos <jordanalsantos@hotmail.com>
2025-09-03 20:01:09 -0300

    arquivo widget

---
+++ Satiros/WidgetMural/WidgetMuralBundle.swift
@@ -0,0 +1,16 @@
+//
+//  WidgetMuralBundle.swift
+//  WidgetMural
+//
+//  Created by Jordana Lourenço Santos on 03/09/25.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct WidgetMuralBundle: WidgetBundle {
+    var body: some Widget {
+        WidgetMural()
+    }
+}
